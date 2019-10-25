29f8384db0fd3a8cf1b0917100a2e40073f80919
Dawood Awny Dawood Alnajjar <dawood.alnajjar@sol11-linux.ABTLUS.org.br>
2019-10-25 16:24:57 -0300

    Adding crio setup beamline folder structure generation script

---
+++ crioSetupBlFolders.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+CRIONAME=$1
+CRIOIOCPATH=$2
+
+if [ -z $CRIONAME ] || [ -z $CRIOIOCPATH ]
+    then
+        echo "CRIO chassi name postfix or latest CRIO IOC folder name not inserted."
+        echo "usage: ./crioSetupBlFolders.sh <CRIO CHASSI NAME POSTFIX> <CRIO IOC FOLDER NAME>"
+        exit
+fi
+
+if [ ! -d /usr/local/epics-nfs/apps/crio-ioc/$CRIOIOCPATH ]
+    then
+        echo "Directory /usr/local/epics-nfs/apps/crio-ioc/$CRIOIOCPATH exists."
+        exit
+fi
+
+mkdir -p $CRIONAME 
+cd $CRIONAME
+mkdir -p epics/apps/config 
+cd epics
+ln -s /usr/local/epics-nfs/base/R3.15.6 base
+cd apps
+ln -s /usr/local/epics-nfs/apps/crio-ioc/$CRIOIOCPATH crio-ioc
