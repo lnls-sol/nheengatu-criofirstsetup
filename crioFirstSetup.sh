9399351d7be3e4238aebf7ea96d0c9e1f2ac7e20
Dawood Alnajjar <dawood.alnajjar@lnls.br>
2020-11-20 11:23:13 -0300

    Fixed some ntp related problems.

--- crioFirstSetup.sh
+++ crioFirstSetup.sh
@@ -28,7 +28,7 @@ cat /etc/opkg/base-feeds.conf
 
 #Update and install useful stuff
 opkg update
-opkg install nfs-utils-client git vim rsync python3 screen ntp ntp-utils ntpdate
+opkg install nfs-utils-client git vim rsync python3 screen ntpdate
 
 
 
@@ -107,7 +107,6 @@ cp files/fstab /etc/network/if-up.d/.
 mount -a
 
 cp files/epics.sh /etc/profile.d
-server 0.natinst.pool.ntp.org
 echo "/usr/local/epics/base/lib/linux-x86_64" > /etc/ld.so.conf.d/epics.conf
 echo "/usr/local/epics-nfs/lib/crio-libs/2020_08_21_01/lib" >> /etc/ld.so.conf.d/epics.conf
 ldconfig
@@ -134,15 +133,17 @@ ln -s /etc/init.d/iocsd /etc/rc5.d/S99iocsd
 
 echo "-------------Fixing date time ---------------"
 rm /etc/natinst/share/localtime
-ln -s /usr/share/zoneinfo/Etc/GMT-3 /etc/natinst/share/localtime
+ln -s /usr/share/zoneinfo/Etc/GMT+3 /etc/natinst/share/localtime
+ntpdate ntp.cnpem.br
+
+opkg install ntp ntp-utils 
 sed -i 's/server 0.natinst.pool.ntp.org//g' /etc/ntp.conf
 sed -i 's/server 1.natinst.pool.ntp.org//g' /etc/ntp.conf
 sed -i 's/server 2.natinst.pool.ntp.org//g' /etc/ntp.conf
 sed -i 's/server 3.natinst.pool.ntp.org//g' /etc/ntp.conf
 echo "server ntp.cnpem.br" >> /etc/ntp.conf
 ntpq -p
-sed -i 's/NTPSERVERS=""/NTPSERVERS="ntp.cnpem.br"/g' /etc/default/ntpdate
-ntpdate ntp.cnpem.br
 
+hwclock -w
 
 reboot
